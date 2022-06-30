f6f3741533838b971bb1ada3fa2251d3b45bf82f
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2022-06-30 03:46:52 -0700

    Fix platforms.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
   name: "WrkstrmLog",
-  platforms: [.iOS(.v12), .macOS(.v11), .watchOS(.v5)],
+  platforms: [
+    .iOS(.v12),
+    .macOS(.v12),
+    .watchOS(.v5)],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to
     // other packages.
