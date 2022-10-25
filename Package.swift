c5a173fe4d12f6bfe407462a5d1494d41bc79c9a
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2022-10-25 03:28:23 -0700

    Add CodeSwiftly to lint step + update lint step.

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
   platforms: [
     .iOS(.v12),
     .macOS(.v12),
-    .watchOS(.v5)],
+    .watchOS(.v5),
+  ],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to
     // other packages.
