53109b90aac10a39494146a8825ad54ceb814161
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-10-22 19:53:25 -0700

    Cleanup swift Package files.

--- Package.swift
+++ Package.swift
@@ -1,17 +1,15 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.8
 import PackageDescription
 
 let package = Package(
   name: "WrkstrmLog",
   platforms: [
-    .iOS(.v12),
+    .iOS(.v15),
     .macOS(.v12),
     .tvOS(.v12),
     .watchOS(.v5),
   ],
   products: [
-    // Products define the executables and libraries produced by a package, and make them visible to
-    // other packages.
     .library(name: "Lumberjack", type: .dynamic, targets: ["Lumberjack"]),
     .library(name: "WrkstrmLog", targets: ["WrkstrmLog"]),
     .library(name: "WSMLogger", type: .dynamic, targets: ["WSMLogger"]),
@@ -20,9 +18,6 @@ let package = Package(
     .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test
-    // suite. Targets can depend on other targets in this package, and on products in packages which
-    // this package depends on.
     .target(name: "Lumberjack", dependencies: []),
     .target(name: "WrkstrmLog", dependencies: [
       .product(name: "Logging", package: "swift-log")
