0acf59dc3ab181de0e480b45733d138d65c523a1
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-10-25 20:52:35 -0700

    Revert "Try Swift 5.7"
    This reverts commit c6216bb1c4fe9f5fd34976deed5e37e7ec3f9daa.

--- Sources/WrkstrmLog/WrkstrmLog.swift
+++ Sources/WrkstrmLog/WrkstrmLog.swift
@@ -8,27 +8,28 @@ let swiftLogger: Logging.Logger = { () -> Logging.Logger in
   return logger
 }()
 
-let osLogger: OSLog = .init(subsystem: "com.wrkstrm.os-log", category: "default")
+let osLogger: OSLog = OSLog(subsystem: "com.wrkstrm.os-log", category: "default")
 
 public enum Log {
-
+  
   public static var style: Log = .use_os_log
-
+  
   public static var maxFunctionLength: Int?
-
+  
   case use_print_log
   case use_os_log
   case use_swift_log
-
+  
   static func formattedFunction(_ function: String) -> String {
-    let functionString: String = if let maxLength = maxFunctionLength {
-      String(function.prefix(maxLength))
+    let functionString: String
+    if let maxLength = maxFunctionLength {
+      functionString = String(function.prefix(maxLength))
     } else {
-      function
+      functionString = function
     }
     return functionString
   }
-
+  
   public static func verbose(
     _ string: String,
     file: String = #file,
@@ -41,7 +42,7 @@ public enum Log {
       .info, emoji: "ℹ️", string: string,
       file: file, function: function, line: line, column: column, dso: dso)
   }
-
+  
   public static func error(
     _ string: String,
     file: String = #file,
@@ -54,7 +55,7 @@ public enum Log {
       .error, emoji: "⚠️", string: string,
       file: file, function: function, line: line, column: column, dso: dso)
   }
-
+  
   public static func `guard`(
     _ string: String = "",
     file: String = #file,
@@ -68,7 +69,7 @@ public enum Log {
       file: file, function: function, line: line, column: column, dso: dso)
     fatalError()
   }
-
+  
   // swiftlint:disable:next function_parameter_count
   static func log(
     _ level: Logging.Logger.Level,
@@ -87,25 +88,22 @@ public enum Log {
     switch style {
     case .use_print_log:
       print("\(emoji) \(fileName):\(String(line))|\(functionString)| " + string)
-
+      
     case .use_os_log:
-      os_log(
-        level.toOSType,
-        dso: dso,
-        log: osLogger,
-        "%s-%i|%s| %s",
-        url.lastPathComponent,
-        line,
-        functionString,
-        string)
+      os_log(level.toOSType,
+             dso: dso,
+             log: osLogger,
+             "%s-%i|%s| %s",
+             url.lastPathComponent,
+             line,
+             functionString,
+             string)
     case .use_swift_log:
-      swiftLogger.log(
-        level: level,
-        "\(line)|\(functionString)| \(string)",
-        source: url.lastPathComponent,
-        file: file,
-        function: functionString,
-        line: line)
+      swiftLogger.log(level: level, "\(line)|\(functionString)| \(string)",
+                        source: url.lastPathComponent,
+                        file: file,
+                        function: functionString,
+                        line: line)
     }
   }
 }
