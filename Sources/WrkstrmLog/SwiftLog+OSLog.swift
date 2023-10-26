c194807d0ae2c36246e4669856aaf37fe8d1581c
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-10-25 20:57:12 -0700

    Add swift-format to repo

--- Sources/WrkstrmLog/SwiftLog+OSLog.swift
+++ Sources/WrkstrmLog/SwiftLog+OSLog.swift
@@ -2,21 +2,27 @@ import os
 import Logging
 
 extension Logging.Logger.Level {
-  
+
   public var toOSType: OSLogType {
     switch self {
     case .critical:
       return .fault
+
     case .info:
       return .info
+
     case .trace:
       return .info
+
     case .debug:
       return .debug
+
     case .error:
       return .error
+
     case .notice:
       return .default
+
     case .warning:
       return .error
     }
