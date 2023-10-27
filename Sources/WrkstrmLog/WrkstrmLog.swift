cb330974fab42cd62583294ae99c80542137a53d
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-10-27 11:01:12 -0700

    Lint steps! (+2 squashed commits) Squashed commits: [4f256e0] Building. [4ae4b35] BASIC bash shell (+2 squashed commits) Squashed commits: [da33957] Update wrkstrm-shell-build.yml [69201ce] Remove deps from localize. Add them to Shell. (+1 squashed commit) Squashed commits: [bec8c50] Add project details for WrkstrmShell

--- Sources/WrkstrmLog/WrkstrmLog.swift
+++ Sources/WrkstrmLog/WrkstrmLog.swift
@@ -2,23 +2,19 @@ import Foundation
 import Logging
 import os
 
-let swiftLogger: Logging.Logger = { () -> Logging.Logger in
-  var logger = Logger(label: "com.wrkstrm.swift-log.default")
-  logger.logLevel = .debug
-  return logger
-}()
+public enum Log: Hashable {
 
-let osLogger = OSLog(subsystem: "com.wrkstrm.os-log", category: "default")
-
-public enum Log {
-
-  public static var style: Log = .os
+  public static var style: Log = .os(system: "com.wrkstrm.os-log", category: "default")
 
   public static var maxFunctionLength: Int?
 
-  case print
-  case os
-  case swift
+  private static var swiftLoggers: [Log: Logging.Logger] = [:]
+
+  private static var osLoggers: [Log: OSLog] = [:]
+
+  case print(system: String, category: String)
+  case os(system: String, category: String)
+  case swift(system: String, category: String)
 
   static func formattedFunction(_ function: String) -> String {
     let functionString: String
@@ -86,22 +82,125 @@ public enum Log {
     let fileName = url.lastPathComponent.replacingOccurrences(of: ".swift", with: "")
     let functionString = formattedFunction(function)
     switch style {
-    case .print:
-      Swift.print("\(emoji) \(fileName):\(String(line))|\(functionString)| " + string)
+    case .print(let system, category: _):
+      Swift.print("\(system)::\(emoji) \(fileName):\(String(line))|\(functionString)| " + string)
 
-    case .os:
+    case let .os(system, category):
+      let logger = Self.osLoggers[
+        Self.style, default: OSLog(subsystem: system, category: category)
+      ]
       os_log(
         level.toOSType,
         dso: dso,
-        log: osLogger,
+        log: logger,
         "%s-%i|%s| %s",
         url.lastPathComponent,
         line,
         functionString,
         string)
 
-    case .swift:
-      swiftLogger.log(
+    case .swift(system: _, category: _):
+      let logger = Self.swiftLoggers[
+        Self.style,
+        default: {
+          var logger = Logger(label: "com.wrkstrm.swift-log.default")
+          logger.logLevel = .debug
+          return logger
+        }()
+      ]
+      logger.log(
+        level: level,
+        "\(line)|\(functionString)| \(string)",
+        source: url.lastPathComponent,
+        file: file,
+        function: functionString,
+        line: line)
+    }
+  }
+
+  public func verbose(
+    _ string: String,
+    file: String = #file,
+    function: String = #function,
+    line: UInt = #line,
+    column: UInt = #column,
+    dso: UnsafeRawPointer = #dsohandle)
+  {
+    log(
+      .info, emoji: "ℹ️", string: string,
+      file: file, function: function, line: line, column: column, dso: dso)
+  }
+
+  public func error(
+    _ string: String,
+    file: String = #file,
+    function: String = #function,
+    line: UInt = #line,
+    column: UInt = #column,
+    dso: UnsafeRawPointer = #dsohandle)
+  {
+    log(
+      .error, emoji: "⚠️", string: string,
+      file: file, function: function, line: line, column: column, dso: dso)
+  }
+
+  public func `guard`(
+    _ string: String = "",
+    file: String = #file,
+    function: String = #function,
+    line: UInt = #line,
+    column: UInt = #column,
+    dso: UnsafeRawPointer = #dsohandle) -> Never
+  {
+    log(
+      .critical, emoji: "❌", string: string,
+      file: file, function: function, line: line, column: column, dso: dso)
+    fatalError()
+  }
+
+  // swiftlint:disable:next function_parameter_count
+  func log(
+    _ level: Logging.Logger.Level,
+    emoji: String,
+    string: String,
+    file: String,
+    function: String,
+    line: UInt,
+    column _: UInt,
+    dso: UnsafeRawPointer)
+  {
+    // swiftlint:disable:next force_unwrapping
+    let url = URL(string: file.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)!
+    let fileName = url.lastPathComponent.replacingOccurrences(of: ".swift", with: "")
+    let functionString = Self.formattedFunction(function)
+    switch self {
+    case .print(let system, category: _):
+      Swift.print("\(system)::\(emoji) \(fileName):\(String(line))|\(functionString)| " + string)
+
+    case let .os(system, category):
+      let logger = Self.osLoggers[
+        Self.style, default: OSLog(subsystem: system, category: category)
+      ]
+      os_log(
+        level.toOSType,
+        dso: dso,
+        log: logger,
+        "%s-%i|%s| %s",
+        url.lastPathComponent,
+        line,
+        functionString,
+        string)
+
+    case .swift(let system, category: _):
+      let logger = Self.swiftLoggers[
+        Self.style,
+        default: {
+          var logger = Logger(label: system)
+          logger.logLevel = .debug
+          return logger
+        }()
+      ]
+      logger.log(
         level: level,
         "\(line)|\(functionString)| \(string)",
         source: url.lastPathComponent,
