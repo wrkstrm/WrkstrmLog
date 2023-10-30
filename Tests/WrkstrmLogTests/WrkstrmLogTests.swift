9a1b4df7896c2424f9ef8953955424eefa28b2be
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-10-30 01:22:51 -0700

    Update WrkstrmLogTests.swift

--- Tests/WrkstrmLogTests/WrkstrmLogTests.swift
+++ Tests/WrkstrmLogTests/WrkstrmLogTests.swift
@@ -4,9 +4,6 @@ import XCTest
 
 final class WrkstrmLogTests: XCTestCase {
   func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct
-    // results.
     Log.error("This is interesting.")
     Log.verbose("This is a log.")
     XCTAssert(true)
