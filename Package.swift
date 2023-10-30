5a5550b66e1b8d76efa29d4263743f59d0580e87
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-10-30 01:19:43 -0700

    Split WrkstrmLogObjc out.

--- Package.swift
+++ Package.swift
@@ -10,20 +10,16 @@ let package = Package(
     .watchOS(.v5),
   ],
   products: [
-    .library(name: "WSMLogger", type: .dynamic, targets: ["WSMLogger"]),
     .library(name: "WrkstrmLog", targets: ["WrkstrmLog"]),
-    .library(name: "Lumberjack", type: .dynamic, targets: ["Lumberjack"]),
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
   ],
   targets: [
-    .target(name: "Lumberjack", dependencies: []),
     .target(
       name: "WrkstrmLog",
       dependencies: [
         .product(name: "Logging", package: "swift-log"),
       ]),
-    .target(name: "WSMLogger", dependencies: ["Lumberjack"]),
     .testTarget(name: "WrkstrmLogTests", dependencies: ["WrkstrmLog"]),
   ])
