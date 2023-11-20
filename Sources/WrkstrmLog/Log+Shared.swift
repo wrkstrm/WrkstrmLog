67ace40ea1f63e11577588cd3b6a32d9838f411d
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-11-19 18:59:23 -0800

    Split the shared logger.

---
+++ Sources/WrkstrmLog/Log+Shared.swift
@@ -0,0 +1,127 @@
+//
+//  File.swift
+//  
+//
+//  Created by Cristian Adalberto Monterroza Calderon on 11/19/23.
+//
+
+import Foundation
+
+extension Log {
+  /// The shared logger instance, often used for default logging.
+  /// This shared instance can be conveniently used throughout your application.
+  ///
+  /// Example usage:
+  /// ```
+  /// Log.shared.info("Application started")
+  /// ```
+  public static var shared = Log(system: "wrkstrm", category: "shared") {
+    didSet {
+      shared.verbose("New Logger: \(shared)")
+    }
+  }
+
+  /// Logs a verbose message with the specified parameters.
+  ///
+  /// - Parameters:
+  ///   - describable: The message string to log.
+  ///   - file: The source file where the log message is generated.
+  ///   - function: The name of the function where the log message is generated.
+  ///   - line: The line number in the source file where the log message is generated.
+  ///   - column: The column number in the source file where the log message is generated.
+  ///   - dso: The address of the shared object where the log message is generated.
+  public static func verbose(
+    _ describable: Any,
+    file: String = #file,
+    function: String = #function,
+    line: UInt = #line,
+    column: UInt = #column,
+    dso: UnsafeRawPointer = #dsohandle)
+  {
+    Log.shared.verbose(describable,
+      file: file,
+      function: function,
+      line: line,
+      column: column,
+      dso: dso)
+  }
+
+  /// Logs a info message with the specified parameters.
+  ///
+  /// - Parameters:
+  ///   - describable: The message string to log.
+  ///   - file: The source file where the log message is generated.
+  ///   - function: The name of the function where the log message is generated.
+  ///   - line: The line number in the source file where the log message is generated.
+  ///   - column: The column number in the source file where the log message is generated.
+  ///   - dso: The address of the shared object where the log message is generated.
+  public static func info(
+    _ describable: Any,
+    file: String = #file,
+    function: String = #function,
+    line: UInt = #line,
+    column: UInt = #column,
+    dso: UnsafeRawPointer = #dsohandle)
+  {
+    Log.shared.info(
+      describable,
+      file: file,
+      function: function,
+      line: line,
+      column: column,
+      dso: dso)
+  }
+
+  /// Logs an error message with the specified parameters.
+  ///
+  /// - Parameters:
+  ///   - describable: The message string to log.
+  ///   - file: The source file where the log message is generated.
+  ///   - function: The name of the function where the log message is generated.
+  ///   - line: The line number in the source file where the log message is generated.
+  ///   - column: The column number in the source file where the log message is generated.
+  ///   - dso: The address of the shared object where the log message is generated.
+  public static func error(
+    _ describable: Any,
+    file: String = #file,
+    function: String = #function,
+    line: UInt = #line,
+    column: UInt = #column,
+    dso: UnsafeRawPointer = #dsohandle)
+  {
+    Log.shared.error(
+      describable,
+      file: file,
+      function: function,
+      line: line,
+      column: column,
+      dso: dso)
+  }
+
+  /// Logs a critical message and triggers a fatal error.
+  ///
+  /// - Parameters:
+  ///   - describable: The message string to log.
+  ///   - file: The source file where the log message is generated.
+  ///   - function: The name of the function where the log message is generated.
+  ///   - line: The line number in the source file where the log message is generated.
+  ///   - column: The column number in the source file where the log message is generated.
+  ///   - dso: The address of the shared object where the log message is generated.
+  /// - Returns: Never, indicating a fatal error.
+  public static func `guard`(
+    _ describable: Any? = nil,
+    file: String = #file,
+    function: String = #function,
+    line: UInt = #line,
+    column: UInt = #column,
+    dso: UnsafeRawPointer = #dsohandle) -> Never
+  {
+    Log.shared.`guard`(
+      describable,
+      file: file,
+      function: function,
+      line: line,
+      column: column,
+      dso: dso)
+  }
+}
